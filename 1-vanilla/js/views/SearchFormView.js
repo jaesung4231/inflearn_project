36ed89f68a93c24d4e292e328b7f06e1ce9cf1ea
jaesung97 <jaesung4231@naver.com>
2023-10-31 21:26:34 +0900

    searchform update

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,14 +1,29 @@
-import { qs } from "../helpers.js";
+import { on, qs } from "../helpers.js";
 import View from "./View.js";
 
+const tag = "[SearchFormView]";
 export default class SearchFormView extends View {
   constructor() {
     super(qs("#search-form-view"));
     this.resetElement = qs("[type=reset]", this.element);
+    this.inputElement = qs("[type=text]", this.element);
     this.showResetButton(false);
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => {
+      this.handleKeyup();
+    });
+  }
+
+  handleKeyup() {
+    const value = this.inputElement.value;
+    console.log(value);
+    this.showResetButton(value.length > 0);
   }
 }
